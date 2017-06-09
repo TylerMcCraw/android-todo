c19ff8d9a181d810f95e5907c4a95715ad1c4ba3
Tyler McCraw <mccraw.tyler@gmail.com>
2017-06-09 16:44:46 -0400

    update firebasepushidgenerator docs

--- app/src/main/java/com/w3bshark/todo/util/FireBasePushIdGenerator.java
+++ app/src/main/java/com/w3bshark/todo/util/FireBasePushIdGenerator.java
@@ -3,15 +3,17 @@ package com.w3bshark.todo.util;
 import java.util.Date;
 
 /**
+ * Created by Tyler McCraw on 6/9/17.
+ * <p/>
  * Fancy ID generator that creates 20-character string identifiers with the following properties:
- * <p>
+ * <p/>
  * 1. They're based on timestamp so that they sort *after* any existing ids.
  * 2. They contain 72-bits of random data after the timestamp so that IDs won't collide with other clients' IDs.
  * 3. They sort *lexicographically* (so the timestamp is converted to characters that will sort properly).
  * 4. They're monotonically increasing.  Even if you generate more than one in the same timestamp, the
  * latter ones will sort after the former ones.  We do this by using the previous random bits
  * but "incrementing" them by 1 (only in the case of a timestamp collision).
- *
+ * <p/>
  * @see <a href="https://gist.github.com/mikelehen/3596a30bd69384624c11#file-generate-pushid-js"> Original port by mikelehen </a>
  */
 public class FireBasePushIdGenerator {
@@ -20,7 +22,7 @@ public class FireBasePushIdGenerator {
 
     public static String generatePushId() {
         // Timestamp of last push, used to prevent local collisions if you push twice in one ms.
-//        long lastPushTime = 0L;
+//        long lastPushTime = 0L; //TODO we should persist the last push time locally to reuse this
 
         // We generate 72-bits of randomness which get turned into 12 characters and
         // appended to the timestamp to prevent collisions with other clients. We store the last
@@ -30,7 +32,7 @@ public class FireBasePushIdGenerator {
 
         long now = new Date().getTime();
 
-//        boolean duplicateTime = (now == lastPushTime);
+//        boolean duplicateTime = (now == lastPushTime); //TODO
         boolean duplicateTime = false;
 
         char[] timeStampChars = new char[8];
